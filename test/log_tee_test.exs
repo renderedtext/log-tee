39bd8d673612dcae04a29451ea0a2f58b95dd352
Predrag Rakic <prakic@renderedtext.com>
2017-01-11 01:32:00 +0100

    add tests

--- test/log_tee_test.exs
+++ test/log_tee_test.exs
@@ -2,7 +2,21 @@ defmodule LogTeeTest do
   use ExUnit.Case
   doctest LogTee
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ExUnit.CaptureLog
+
+
+  defmacro log_tee_test(severity) do
+    quote do
+      f = fn -> LogTee.unquote(severity)(12, "6+6") end
+      assert capture_log(f) =~ "[#{unquote(severity)}] "
+      assert capture_log(f) =~ " 6+6: 12"
+      assert f.() == 12
+    end
   end
+
+  test "debug" do log_tee_test(:debug) end
+  test "info"  do log_tee_test(:info) end
+  test "warn"  do log_tee_test(:warn) end
+  test "error" do log_tee_test(:error) end
 end
+
